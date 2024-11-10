463519393c4e8f9517bd36a8affef59ff89e2219
adityawintech <aditya#19AI>
2024-11-10 21:59:49 +0530

    okkkm

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,12 @@
 
 'use client'
-
-import { LayoutEl } from "@/shared/layoutEl";
 import { Home } from '../components/Home'
 export default function Page() {
   return (
     <>
-      <LayoutEl>
+ 
         <Home />
-      </LayoutEl>
+      
     </>
   );
 }
---
+++ src/elements/HomeEl/ShopEl/index.jsx
@@ -0,0 +1,12 @@
+
+
+const ShopEl=()=>{
+    const display=(
+     <>
+     <h1>Homepage</h1>
+     <h1>jkjfjekfekigf</h1>
+     </>
+    )
+    return display
+}
+export default ShopEl
