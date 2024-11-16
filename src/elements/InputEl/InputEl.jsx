08d1341ce744a2a339201ba7e8de2c1ffd7ffc08
utkarshwebitya <uks797978@gmail.com>
2024-11-16 19:02:01 +0530

    login and registration working

--- src/elements/InputEl/InputEl.jsx
+++ src/elements/InputEl/InputEl.jsx
@@ -2,20 +2,15 @@
 
 import { useState } from "react";
 
-const Input = ({ type,name,ph }) => {
-    const [inputValue, setInputValue] = useState('');
+const Input = ({ type,name,ph,data,onChange }) => {
 
-    const hanldeChange= (event) => {
-        const value= event.currentTarget.value
-        setInputValue(value);
-    }
     return(
         <input 
             type={type}
             name={name}
             placeholder={ph}
-            value={inputValue}
-            onChange={hanldeChange}
+            value={data}
+            onChange={onChange}
             className="outline-none border shadow-sm py-1 px-2 text-sm rounded-md focus:border-[] w-full"
         />
     );
