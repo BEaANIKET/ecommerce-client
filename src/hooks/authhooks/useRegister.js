f4a6d6bc19fae1fd90b71ea3c76143b21ec2f070
BEaANIKET <aniketchaturvedi309@gmail.com>
2024-11-18 16:58:35 +0530

    auth complete

---
+++ src/hooks/authhooks/useRegister.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import axios from "axios";
+import { useReuestApi } from "../useRequestApi";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+
+export const useRegister = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [success, setSuccess] = useState(false);
+    const router = useRouter();
+
+    const register = async ({ first_name, last_name, email, password }) => {
+        setIsLoading(true);
+        setError(null);
+        setSuccess(false);
+
+        try {
+            const response = await useReuestApi('api/auth/register', 'POST', { name: `${first_name} ${last_name}`, email, password });
+            setSuccess(true);
+            toast.success("Successfully registered")
+            router.replace('/login')
+
+        } catch (err) {
+            setError(err.response?.data?.message || "Failed to register.");
+            toast.error(err.response?.data?.message || "Failed to register.")
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    return { register, isLoading, error, success };
+};
--- src/hooks/useRequestApi.js
+++ src/hooks/useRequestApi.js
@@ -1,27 +1,26 @@
 import axios from "axios"
 
-export const useReuestApi= async (
+export const useReuestApi = async (
     endpoint,
-    method = 'GET', 
+    method = 'GET',
     body
 ) => {
-    const token= localStorage.getItem('token');
+    const token = localStorage.getItem('token');
     console.log(`${process.env.NEXT_PUBLIC_BASE_URL}/${endpoint}`)
-    try{
-        
-        const response= await axios({
-            url:`${process.env.NEXT_PUBLIC_BASE_URL}/${endpoint}`,
+    try {
+
+        const response = await axios({
+            url: `${process.env.NEXT_PUBLIC_BASE_URL}/${endpoint}`,
             method: method,
             data: body || undefined,
-            headers:{
+            headers: {
                 'Content-Type': 'application/json',
                 Authorization: `Bearer ${token}`
             },
             withCredentials: true
         });
         return response.data;
-    }catch(error){
-        console.log(error)
+    } catch (error) {
         throw error;
     }
 
